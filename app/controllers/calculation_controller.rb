054265d07be86478744ca81d0112dd0c0f7511cb
GHauer <hauer.georg@gmail.com>
2014-05-12 16:43:38 -0500

    Calculation controller changes

--- app/controllers/calculation_controller.rb
+++ app/controllers/calculation_controller.rb
@@ -7,6 +7,26 @@ class CalculationController < ApplicationController
     @answer = Math.sqrt(@the_number)
   end
 
-  def pmt
+  def sq
+    @the_number = params[:number].to_f
+    @answer = (@the_number)*(@the_number)
+    @answeri = @answer.to_i
+    @the_numberi = @the_number.to_i
   end
+
+  def sqrt
+    @the_number = params[:number].to_f
+    @answer = Math.sqrt(@the_number)
+    @answeri = @answer.to_i
+    @the_numberi = @the_number.to_i
+  end
+
+  def pmt
+    @rate = (params[:interest_rate].to_f)/100
+    @nper = params[:number_of_payments].to_f
+    @pval = params[:present_value].to_f
+    @payment = @pval * (@rate * ((1 + @rate) ** @nper) / ((1 + @rate) ** @nper - 1))
+    @answer = @payment.to_i
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,3 +3,4 @@ Rails.application.routes.draw do
   get("/square_root/:number", { :controller => "calculations", :action => "sqrt" })
   get("/payment/:interest_rate/:number_of_payments/:present_value", { :controller => "calculations", :action => "pmt" })
 end
+
